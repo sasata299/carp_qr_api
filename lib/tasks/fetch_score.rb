fa64edef34b6a988cf0a9e55e1e45090e6d4ed09
sasata299 <karaage299@gmail.com>
2016-08-27 08:40:21 +0900

    事前処理をsetupメソッドとして実装する

--- lib/tasks/fetch_score.rb
+++ lib/tasks/fetch_score.rb
@@ -1,6 +1,8 @@
 class Tasks::FetchScore
   require 'capybara/poltergeist'
 
+  @doc = nil
+
   class << self
     def score(doc)
       scores = []
@@ -24,22 +26,12 @@ class Tasks::FetchScore
     end
 
     def execute
-      Capybara.register_driver :poltergeist do |app|
-        Capybara::Poltergeist::Driver.new(app, js_errors: false, timeout: 5000)
-      end
+      setup
 
-      session = Capybara::Session.new(:poltergeist)
-
-      session.driver.headers = {
-        'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2564.97 Safari/537.36'
-      }
-      session.visit 'http://score.rcc.jp/'
-      doc = Nokogiri::HTML.parse(session.html)
-
-      game = Game.new(doc)
+      game = Game.new(@doc)
 
       # 試合終了時には試合結果を保存する
-      game_score = score(doc) unless doc.css('table.pitcher').empty?
+      game_score = score(@doc) unless @doc.css('table.pitcher').empty?
       puts game_score
 
       # 得点経過の要素を持ってくる
@@ -57,6 +49,22 @@ class Tasks::FetchScore
         prompt_report.detail = data[2]
         prompt_report.save!
       end
+    end
+
+    private
+
+    def setup
+      Capybara.register_driver :poltergeist do |app|
+        Capybara::Poltergeist::Driver.new(app, js_errors: false, timeout: 5000)
+      end
+
+      session = Capybara::Session.new(:poltergeist)
+      session.driver.headers = {
+        'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2564.97 Safari/537.36'
+      }
+      session.visit 'http://score.rcc.jp/'
+
+      @doc = Nokogiri::HTML.parse(session.html)
     end
   end
 end
