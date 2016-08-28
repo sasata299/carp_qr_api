23cfa721117c24acda274c5e08c48155b9eedb42
sasata299 <karaage299@gmail.com>
2016-08-28 12:09:14 +0900

    日付の指定をbefore_actionに移動する

--- app/controllers/score_reports_controller.rb
+++ app/controllers/score_reports_controller.rb
@@ -1,8 +1,14 @@
 class ScoreReportsController < ApplicationController
-  def index
-    date = Date.yesterday
+  before_action :set_target_date
 
-    @score_reports = ScoreReport.active.where(game_date: date)
-    @result = Result.active.find_by(game_date: date)
+  def index
+    @score_reports = ScoreReport.active.where(game_date: @date)
+    @result = Result.active.find_by(game_date: @date)
+  end
+
+  private
+
+  def set_target_date
+    @date = Date.yesterday
   end
 end
